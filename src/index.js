23401f022dfce264a70613651c10294d01f23e5a
advait-cs <1962advaitad@gmail.com>
2022-01-31 16:38:58 +0530

    added note functionalities

--- src/index.js
+++ src/index.js
@@ -6,8 +6,5 @@ import App from "./components/app"
 ReactDom.render(<App />,document.getElementById("root"));
 
 
-//6. Make sure that the final website is styled like the example shown here:
-//https://l1pp6.csb.app/
 
-//HINT: You will need to study the classes in teh styles.css file to appy styling.
 
